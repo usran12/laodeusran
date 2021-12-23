e8f7e97fd71cd0f7a829d5ae1a08ee9d971ccc5c
usran12 <96564949+usran12@users.noreply.github.com>
2021-12-23 14:43:22 +0800

    Add files via upload

---
+++ phppertama.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata</title>
+</head>
+<style>
+
+    table {
+        border-spacing: 0;
+        margin: 150px 0;
+    }
+    table th {
+        border: 2px solid #aaa;
+        width: 500px;
+        height: 30px;
+    }
+    table td {
+        border: 1px solid #ccc;
+        height: 30px;
+        padding:0 10px;
+        text-align: center;
+    }
+</style>
+<body>
+   <center>
+    <table>
+            <thead>
+                 <th colspan="3" >BIODATA</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <?php
+                        $th = "<td>";
+                        $tht = "</td>";
+                        echo $th . "1" . $tht;
+                        echo $th . "Nama" . $tht;
+                        echo $th . "la ode usran" . $tht;
+                    ?>
+                </tr>
+                <tr>
+                    <?php
+                        $th = "<td>";
+                        $tht = "</td>";
+                        echo $th . "2" . $tht;
+                        echo $th . "Nim" . $tht;
+                        echo $th . "19024014048" . $tht;
+                    ?>
+                </tr>
+                <tr>
+                    <?php
+                        $th = "<td>";
+                        $tht = "</td>";
+                        echo $th . "3" . $tht;
+                        echo $th . "kelas" . $tht;
+                        echo $th . "A2" . $tht;
+                    ?>
+                </tr>
+                <tr>
+                    <?php
+                        $th = "<td>";
+                        $tht = "</td>";
+                        echo $th . "4" . $tht;
+                        echo $th . "Hoby" . $tht;
+                        echo $th . "Ngopi, Nonton " . $tht;
+                    ?>
+                </tr>
+                <tr>
+                    <?php
+                        $th = "<td>";
+                        $tht = "</td>";
+                        echo $th . "5" . $tht;
+                        echo $th . "Alamat" . $tht;
+                        echo $th . "Buton " . $tht;
+                    ?>
+                </tr>
+            </tbody>
+        </table>
+   </center>
+    
+</body>
+</html>
